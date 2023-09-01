5555d4510897c29b65ce6a323e76ea9867deaf48
MAHAMUD <ABDIFATAH90@GMAIL.COM>
2023-08-31 17:42:18 -0700

    Demonstrating multiple files

---
+++ app.js
@@ -0,0 +1,16 @@
+'use strict';
+console.log("I am in the HTML");
+alert (" Welcome to Sky Academy!");
+const usersName = prompt ("What is your name?");
+console.log (usersName);
+if (usersName=="Hassan")
+{alert("Hi Hassan");} 
+else { 
+    alert("Glad to have you here")}
+
+document.write("Hi " + usersName + "welcome to my page!");
+
+const color = prompt ("What is your favorite color?");
+
+
+
